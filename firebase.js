c2d13058fae1ab6d9dbcb5ae837ec81e950f55d5
Amina Sagheer <172102325+Amina-Sagheer@users.noreply.github.com>
2025-05-22 20:06:59 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDVfYLJfIMaELU4kiO7hysKTkLObgqxLjQ",
-  authDomain: "pantryapp-2c3b2.firebaseapp.com",
-  projectId: "pantryapp-2c3b2",
-  storageBucket: "pantryapp-2c3b2.appspot.com",
-  messagingSenderId: "1057145092574",
-  appId: "1:1057145092574:web:c034e0ad8cf0213160854f"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
